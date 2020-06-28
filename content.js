d8acb7bb2e171e98a604bff036cb10384ca72eeb
Alexandre Guimenti <alexguimenti@gmail.com>
2020-06-28 14:36:17 -0300

    minus and plus buttons

--- content.js
+++ content.js
@@ -1,24 +1,58 @@
 const waitingForHeader = setInterval(() => {
   const header = document.querySelector('._1QUKR');
   if (header) {
-    console.log(header);
+    let speed = 1.00;
     clearInterval(waitingForHeader)
 
-    const button = document.createElement("button");
-    button.innerHTML = "2x"
-    button.classList.add("twoTimesButton");
+    const speedContainer = document.createElement("div");
+    speedContainer.classList.add("speedContainer");
+    header.appendChild(speedContainer)
 
-    button.addEventListener("click", () => {
-      const audios = document.querySelectorAll("audio");
+    const minusButton = document.createElement("button")
+    minusButton.innerHTML = "-"
+    minusButton.classList.add("speedButton")
+    speedContainer.appendChild(minusButton)
+
+    const speedDisplay = document.createElement("span");
+    speedDisplay.innerHTML = speed.toFixed(2);
+    speedDisplay.classList.add("speedDisplay");
+    speedContainer.appendChild(speedDisplay)
+
+    const plusButton = document.createElement("button")
+    plusButton.innerHTML = "+"
+    plusButton.classList.add("speedButton")
+    speedContainer.appendChild(plusButton)
+
+    minusButton.addEventListener("click", () => {
+      console.log("minus");
+      speed -= 0.25;
+      speedDisplay.innerHTML = speed.toFixed(2);
+      let audios = document.querySelectorAll("audio");
       audios.forEach(audio => {
-        audio.playbackRate = 2;
+        audio.playbackRate = speed;
       })
     })
 
-    header.appendChild(button)
+    plusButton.addEventListener("click", () => {
+      console.log("plus")
+      speed += 0.25;
+      speedDisplay.innerHTML = speed.toFixed(2);
+      let audios = document.querySelectorAll("audio");
+      audios.forEach(audio => {
+        audio.playbackRate = speed;
+      })
+    })
 
+    // button.addEventListener("click", () => {
+    //   const audios = document.querySelectorAll("audio");
+    //   audios.forEach(audio => {
+    //     audio.playbackRate = 2;
+    //   })
+    // })
 
   }
 }, 1000)
+
+
 
 
